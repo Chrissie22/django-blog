bb22f926bbc8528f459e9115f660d3aa1bf8c0f8
Chrissie22 <christabel.ojobolo@gmail.com>
2025-11-23 06:35:24 +0100

    Update Blog and Category models for new fields

--- blogs/models.py
+++ blogs/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
-    Category_name = models.CharField(max_length=100, unique=True)
-    created_at = models.DateTimeField(auto_now=True)
+    category_name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     
@@ -13,7 +13,7 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
         
     def __str__(self):
-        return self.Category_name
+        return self.category_name
     
     
 STATUS_CHOICES = (
@@ -27,7 +27,7 @@ class Blog(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
-    short_description = models.TextField(max_length=50)
+    short_description = models.TextField(max_length=200)
     blog_body = models.TextField(max_length=2000)
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="Draft")
     is_featured = models.BooleanField(default=False)
